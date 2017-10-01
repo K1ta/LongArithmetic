0405f3fb06b92be4bfbc79d47640430c042acb88
K1ta <kobalt31@mail.ru>
2017-10-01 18:16:40 +0300

    update

--- src/source/LongNumber.java
+++ src/source/LongNumber.java
@@ -1,135 +1,153 @@
 package source;
 
 public class LongNumber {
-    public long[] numbers;
+    private long[] numbers;
+    private int length;
+    private int bitCount;
 
-    public LongNumber() {
-        numbers = new long[4];
+    public LongNumber(int n) {
+        numbers = new long[n];
+        length = n;
+        countNumbers();
     }
 
-    public LongNumber(long l) {
-        numbers = new long[4];
-        numbers[3] = l;
+    public LongNumber(int n, long value) {
+        numbers = new long[n];
+        length = n;
+        numbers[length - 1] = value;
+        countNumbers();
     }
 
-    public LongNumber(long l1, long l2, long l3, long l4) {
-        numbers = new long[]{l1, l2, l3, l4};
+    public int getLength() {
+        return length;
+    }
+
+    public int getBitCount() {
+        return bitCount;
     }
 
     public long getBit(int n) {
-        int index = 3 - n / 64;
-        long number = n % 64;
-        return (numbers[index] >> number) & 1L;
+        return (n > bitCount - 1) ? 0 : ((numbers[length - (n / 64) - 1] >> (n % 64)) & 1L);
     }
 
-    public void setBit(int n) {
-        int index = 3 - n / 64;
-        int number = n % 64;
-        numbers[index] = numbers[index] | (1L << number);
+    public void setBit(int n, long bit) {
+        numbers[length - (n / 64) - 1] &= ~(1L << (n % 64));
+        numbers[length - (n / 64) - 1] |= (bit << (n % 64));
     }
 
-    public void leftOffset(int index, int n) {
-        numbers[index] = numbers[index] << n;
+    public long getNumber(int n)
+    {
+        return (n >= length) ? 0 : numbers[n];
     }
 
-    public void printNumbers() {
-        for (int j = 127; j >= 0; j--) {
-            if (getBit(j) != 0) {
-                System.out.print(1);
-            } else {
-                System.out.print(0);
-            }
-            if (j % 4 == 0) {
-                System.out.print(" ");
-            }
-            if(j % 64 == 0)
-            {
-                System.out.print("  ");
-            }
+    public void setNumber(int n, long number)
+    {
+        numbers[n] = number;
+    }
+
+    public void leftOffset(long bit)
+    {
+        if(length == 0)
+        {
+            return;
         }
-        System.out.println();
+        for(int i = 0; i < length - 1; i++)
+        {
+            numbers[i] = numbers[i] << 1;
+            setBit(64*(length - 1 - i), 64 * (length - 1 - i) - 1);
+        }
+        numbers[length - 1] = numbers[length - 1] << 1;
+        setBit(0,bit);
+    }
+
+    public void rightOffset(long bit)
+    {
+        if(length == 0)
+        {
+            return;
+        }
+        for(int i = length - 1; i > 0; i--)
+        {
+            numbers[i] = numbers[i] >> 1;
+            setBit(64*(i - 1) - 1, 64 * (i - 1));
+        }
+        numbers[0] = numbers[0] >> 1;
+        setBit(64 * (length - 1) - 1,bit);
+    }
+
+    public void rightOffset(int n, long bit)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            rightOffset(bit);
+        }
+    }
+
+    public void countNumbers() {
+        int i = length * 64 - 1;
+        while (i >= 0 && ((numbers[length - (i / 64) - 1] >> (i % 64)) & 1L) == 0) {
+            i--;
+        }
+        bitCount = (i == -1) ? 0 : (i + 1);
+    }
+
+    public void add(long l)
+    {
+        for(int i = 0; i < length; i++)
+        {
+            numbers[i] += l;
+        }
     }
 
     public void print() {
-        //всего 80 тетрад
-        long[] tetras = new long[5];
-        boolean[] correction = new boolean[80];
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 64; j += 4) {
-                tetras[i] = tetras[i] | (3L << j);
-            }
+        //Определяем количество тетрад для двоично-десятичного кода
+        int tetradsCount = (int)Math.ceil((bitCount * 0.3));
+        //Тетрады храним в лонгах
+        LongNumber tetrads = new LongNumber(tetradsCount / 16 + tetradsCount % 16);
+        //Прибавляем к каждой тетраде тройку
+        for(int i = 0; i < tetrads.length; i++)
+        {
+            tetrads.setNumber(i, 3689348814741910323L);
         }
-        //алгоритм
-        for (int i = 255; i >= 0; i--) {
-            int corrInd = 0;
-            //делаем сдвиг тетрад и проверяем коррекцию
-            for (int j = 0; j < 5; j++) {
-                //проверка на необходимость коррекции
-                for (int k = 0; k < 64; k += 4) {
-                    if ((tetras[j] & (1L << (63 - k))) != 0) {
-                        correction[corrInd] = true;
-                    } else {
-                        correction[corrInd] = false;
-                    }
-                    corrInd++;
-                }
-                tetras[j] = tetras[j] << 1;
-                if (j < 4) {
-                    if ((tetras[j + 1] & (1L << 63)) != 0) {
-                        tetras[j] += 1;
-                    }
-                } else {
-                    tetras[j] = tetras[j] & ~1L;
-                    tetras[j] += getBit(i);
-                }
-            }
-            //проводим коррекцию
-            corrInd = 0;
-            for (int j = 0; j < 5; j++) {
-                for (int k = 0; k < 64; k += 4) {
-                    long corr = (3L << (60 - k));
-                    if (correction[corrInd] == true) {
-                        tetras[j] += corr;
-                    } else {
-                        tetras[j] -= corr;
-                    }
-                    corrInd++;
-                }
-            }
-        }
-        //обратно
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 64; j += 4) {
-                long corr = (3L << (64 - j));
-                tetras[i] -= corr;
-            }
-        }
-        //битовый вывод
-        /*for (int _i = 0; _i < 5; _i++) {
-            for (int j = 63; j >= 0; j--) {
-                if ((tetras[_i] & (1L << j)) != 0) {
-                    System.out.print(1);
-                } else {
-                    System.out.print(0);
-                }
-                if (j % 4 == 0) {
+        //Главный цикл
+        for(int i = bitCount - 1; i >= 0; i--)
+        {
+            System.out.println("i: " + i);
+            tetrads.countNumbers();
+            for(int j = tetrads.bitCount - 1; j >= 0; j--)
+            {
+                System.out.print(tetrads.getBit(j));
+                if(j % 4 == 0)
+                {
                     System.out.print(" ");
                 }
             }
-            System.out.print("  ");
+            System.out.println();
+            //создаем LongNumber для коррекции
+            LongNumber correction = lArithmetic.AND(tetrads,-8608480567731124088L);
+            //сдвигаем тетрады и толкаем в них бит из числа
+            tetrads.leftOffset(getBit(i));
+            LongNumber _correction = correction;
+            _correction.rightOffset(2, 0);
+            //определяем, нужна ли коррекция
+            correction = lArithmetic.OR(correction, _correction);
+            correction.add(3689348814741910323L);
+            //корректируем
+            tetrads = lArithmetic.sum(tetrads, correction);
         }
-        System.out.println();*/
-        //десятичный вывод
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 64; j += 4) {
-                long res = (((tetras[i] >> (63 - j)) & 1) * 8) +
-                        (((tetras[i] >> (63 - j - 1)) & 1) * 4) +
-                        (((tetras[i] >> (63 - j - 2)) & 1) * 2) +
-                        ((tetras[i] >> (63 - j - 3)) & 1);
-                System.out.print(res);
+        for(int i = 0; i < tetrads.length; i++)
+        {
+            tetrads.numbers[i] -= 2459565876494606883L;
+        }
+        System.out.println("result:");
+        tetrads.countNumbers();
+        for(int i = tetrads.bitCount - 1; i >= 0; i--)
+        {
+            System.out.print(tetrads.getBit(i));
+            if(i % 4 == 0)
+            {
+                System.out.print(" ");
             }
-            System.out.print(" ");
         }
-        System.out.println();
     }
 }
--- src/source/lArithmetic.java
+++ src/source/lArithmetic.java
@@ -2,57 +2,39 @@ package source;
 
 public class lArithmetic {
 
-    public static LongNumber AND(LongNumber a, LongNumber b) {
-        return new LongNumber(a.numbers[0] & b.numbers[0],
-                a.numbers[1] & b.numbers[1],
-                a.numbers[2] & b.numbers[2],
-                a.numbers[3] & b.numbers[3]);
-    }
-
-    public static LongNumber OR(LongNumber a, LongNumber b) {
-        return new LongNumber(a.numbers[0] | b.numbers[0],
-                a.numbers[1] | b.numbers[1],
-                a.numbers[2] | b.numbers[2],
-                a.numbers[3] | b.numbers[3]);
-    }
-
-    public static LongNumber XOR(LongNumber a, LongNumber b) {
-        return new LongNumber(a.numbers[0] ^ b.numbers[0],
-                a.numbers[1] ^ b.numbers[1],
-                a.numbers[2] ^ b.numbers[2],
-                a.numbers[3] ^ b.numbers[3]);
-    }
-
-    public static boolean isZero(LongNumber l) {
-        return ((l.numbers[3] == 0) && (l.numbers[2] == 0) && (l.numbers[1] == 0) && (l.numbers[0] == 0));
-    }
-
-    public static LongNumber leftOffset(LongNumber l) {
-        l.leftOffset(0, 1);
-        if (l.getBit(191) == 1) {
-            l.setBit(192);
-        }
-        l.leftOffset(1, 1);
-        if (l.getBit(127) == 1) {
-            l.setBit(128);
-        }
-        l.leftOffset(2, 1);
-        if (l.getBit(63) == 1) {
-            l.setBit(64);
-        }
-        l.leftOffset(3, 1);
-        return l;
-    }
-
     public static LongNumber sum(LongNumber a, LongNumber b) {
-        LongNumber o1;
-        LongNumber o2;
-        while ((!isZero(AND(a,b)))) {
-            o1 = XOR(a, b);
-            o2 = leftOffset(lArithmetic.AND(a,b));
-            a = o1;
-            b = o2;
+        int maxLength = Math.max(a.getLength(), b.getLength());
+        if (a.getBitCount() - maxLength * 64 == 0 || b.getBitCount() - maxLength * 64 == 0) {
+            maxLength++;
         }
-        return OR(a,b);
+        LongNumber res = new LongNumber(maxLength);
+        int n = Math.max(a.getBitCount(), b.getBitCount());
+        long temp = 0;
+        for (int i = 0; i < n + 1; i++) {
+            res.setBit(i, a.getBit(i) ^ b.getBit(i) ^ temp);
+            temp = (a.getBit(i) & b.getBit(i)) | (a.getBit(i) & temp) | (b.getBit(i) & temp);
+        }
+        res.countNumbers();
+        return res;
+    }
+
+    public static LongNumber AND(LongNumber a, long b)
+    {
+        LongNumber res = new LongNumber(a.getLength());
+        for(int i = 0; i < res.getLength(); i++)
+        {
+            res.setNumber(i, a.getNumber(i) & b);
+        }
+        return res;
+    }
+
+    public static LongNumber OR(LongNumber a, LongNumber b)
+    {
+        LongNumber res = new LongNumber(Math.max(a.getLength(), b.getLength()));
+        for(int i = 0; i < res.getLength(); i++)
+        {
+            res.setNumber(i, a.getNumber(i) | b.getNumber(i));
+        }
+        return res;
     }
 }

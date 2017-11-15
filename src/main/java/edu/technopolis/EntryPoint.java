b335ed4e0918c6f6f0961974a5c6401dd9b636e6
K1ta <kobalt31@mail.ru>
2017-11-15 23:59:43 +0300

    new algorithm

---
+++ src/main/java/edu/technopolis/EntryPoint.java
@@ -0,0 +1,23 @@
+package edu.technopolis;
+
+/**
+ * Это просто точка входа для первого домашнего задания.
+ * Содержит только метод main и ничего кроме. Никакой дполнительной нагрузки не несёт.
+ * Является интерфейсом для
+ * <ul>
+ *     <li>Демонстрации возможности метода main в интрефейсе</li>
+ *     <li>Экономии слова public</li>
+ * </ul>
+ */
+public interface EntryPoint {
+    static void main(String... args) {
+        long time = System.currentTimeMillis();
+        FibonacciAlgorithm algorithm = new DummyFibonacciAlgorithm();
+        System.out.println(algorithm.evaluate(Integer.parseInt(args[0])));
+        System.out.println(System.currentTimeMillis() - time);
+        time = System.currentTimeMillis();
+        FibonacciAlgorithm myAlgorithm = new MyFibonacciAlgorithm();
+        System.out.println(myAlgorithm.evaluate(Integer.parseInt(args[0])));
+        System.out.println(System.currentTimeMillis() - time);
+    }
+}
